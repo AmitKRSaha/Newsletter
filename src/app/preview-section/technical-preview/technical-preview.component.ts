70d5326c1a53c69e1301e696a041fd4b91a2b161
LL\amisaha <amisaha@publicisgroupe.net>
2018-06-13 10:34:42 +0530

    Fixed Minor Issues

--- src/app/preview-section/technical-preview/technical-preview.component.ts
+++ src/app/preview-section/technical-preview/technical-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProgressBarService } from '../../progress-bar.service';
 
 @Component({
   selector: 'app-technical-preview',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnicalPreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public progressBarService: ProgressBarService ) { }
 
   ngOnInit() {
   }
