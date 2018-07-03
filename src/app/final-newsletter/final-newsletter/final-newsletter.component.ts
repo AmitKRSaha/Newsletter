4909a377637d98f1c2853c112fb601df87cd87fb
LL\amisaha <amisaha@publicisgroupe.net>
2018-07-03 16:08:16 +0530

    Updated preview section styles and final newsletter

--- src/app/final-newsletter/final-newsletter/final-newsletter.component.ts
+++ src/app/final-newsletter/final-newsletter/final-newsletter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProgressBarService } from '../../progress-bar.service';
 
 @Component({
   selector: 'app-final-newsletter',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalNewsletterComponent implements OnInit {
 
-  constructor() { }
+  constructor( public progressbar: ProgressBarService ) { }
 
   ngOnInit() {
   }
