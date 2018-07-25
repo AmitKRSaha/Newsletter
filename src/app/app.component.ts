0d1319f790ad41ff3322e9ebc3837606d0997c9b
LL\amisaha <amisaha@publicisgroupe.net>
2018-07-25 11:21:01 +0530

    refactored line logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import * as html2canvas from 'html2canvas';
 import * as $ from 'jquery';
 import { completedSections } from '../app/models/completed-section';
 import { expandtoggle } from '../app/models/toggle-options';
+import { line } from '../app/models/line';
 
 
 @Component({
@@ -19,41 +20,7 @@ export class AppComponent implements OnInit {
 
   expandtoggle = expandtoggle;
 
-  line: any = {
-    sectionHead: {
-      status: 'inactive'
-    },
-    businessupdate: {
-      status: 'inactive'
-    },
-    opportunityInPipeline: {
-      status: 'inactive'
-    },
-    acheivement: {
-      status: 'inactive'
-    },
-    planned: {
-      status: 'inactive'
-    },
-    projstatInPipeline: {
-      status: 'inactive'
-    },
-    tech: {
-      status: 'inactive'
-    },
-    brainteaser: {
-      status: 'inactive'
-    },
-    healthtips: {
-      status: 'inactive'
-    },
-    brainteaserans: {
-      status: 'inactive'
-    },
-    teamandbirthday: {
-      status: 'inactive'
-    }
-  };
+  line: any = line
 
 
   constructor(private progressBarService: ProgressBarService) {
---
+++ src/app/models/line.ts
@@ -0,0 +1,35 @@
+export const line: any = {
+  sectionHead: {
+    status: 'inactive'
+  },
+  businessupdate: {
+    status: 'inactive'
+  },
+  opportunityInPipeline: {
+    status: 'inactive'
+  },
+  acheivement: {
+    status: 'inactive'
+  },
+  planned: {
+    status: 'inactive'
+  },
+  projstatInPipeline: {
+    status: 'inactive'
+  },
+  tech: {
+    status: 'inactive'
+  },
+  brainteaser: {
+    status: 'inactive'
+  },
+  healthtips: {
+    status: 'inactive'
+  },
+  brainteaserans: {
+    status: 'inactive'
+  },
+  teamandbirthday: {
+    status: 'inactive'
+  }
+};
