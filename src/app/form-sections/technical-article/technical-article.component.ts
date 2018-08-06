3d078ec37fece668fff3f7fee612b762ce6653c3
LL\amisaha <amisaha@publicisgroupe.net>
2018-08-06 16:15:13 +0530

    Updated backgroundimage for technical article

--- src/app/form-sections/technical-article/technical-article.component.ts
+++ src/app/form-sections/technical-article/technical-article.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl,  FormGroup, FormArray } from '@angular/forms';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { ProgressBarService } from '../../progress-bar.service';
 import { BrainTeasersComponent } from '../brain-teasers/brain-teasers.component';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-technical-article',
@@ -18,7 +19,7 @@ export class TechnicalArticleComponent implements OnInit {
   @Input() line: any;
   @Input() technicalstat: any;
 
-
+  public uploader: FileUploader = new FileUploader({ url: 'http://localhost:3001/upload' });
 
   userFormuserProjstat = new FormGroup({
     users: new FormArray([
@@ -57,5 +58,23 @@ export class TechnicalArticleComponent implements OnInit {
 
   expandMoreOrLess() {
     this.expandtoggle.technicalsection.status = (this.expandtoggle.technicalsection.status === 'open') ? 'closed' : 'open';
+  }
+
+  backgroundImageUpload() {
+    this.uploader.uploadAll();
+    this.uploader.onErrorItem = (item, response, status, headers) => this.onErrorItem(item, response, status, headers);
+    this.uploader.onSuccessItem = (item, response, status, headers) => this.onSuccessItem(item, response, status, headers);
+
+  }
+
+  onSuccessItem(item: any, response: string, status: number, headers: any): any {
+    const data = JSON.parse(response); // success server response
+    this.progressBarService.addBackgroundImage({ 'technical': data.path });
+    console.log(data);
+  }
+
+  onErrorItem(item: any, response: string, status: number, headers: any): any {
+    const error = JSON.parse(response); // error server response
+    console.log(error);
   }
 }
