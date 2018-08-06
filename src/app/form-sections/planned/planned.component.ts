b7b3ae686497bb28327f7973d0a38ffac371d6ee
LL\amisaha <amisaha@publicisgroupe.net>
2018-08-06 16:02:40 +0530

    Updated back ground image from planned section

--- src/app/form-sections/planned/planned.component.ts
+++ src/app/form-sections/planned/planned.component.ts
@@ -3,6 +3,7 @@ import { FormControl,  FormGroup, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProgressBarService } from '../../progress-bar.service';
 import { ProjectStatisticsComponent } from '../project-statistics/project-statistics.component';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-planned',
@@ -16,7 +17,7 @@ export class PlannedComponent {
   @Input() expandtoggle: any;
   @Input() line: any;
 
-
+  public uploader: FileUploader = new FileUploader({url: 'http://localhost:3001/upload'});
 
   userFormuserplanned = new FormGroup({
     users: new FormArray([
@@ -55,5 +56,21 @@ export class PlannedComponent {
     this.expandtoggle.planned.status = (this.expandtoggle.planned.status === 'open') ? 'closed' : 'open';
   }
 
+  backgroundImageUpload() {
+    this.uploader.uploadAll();
+    this.uploader.onErrorItem = (item, response, status, headers) => this.onErrorItem(item, response, status, headers);
+    this.uploader.onSuccessItem = (item, response, status, headers) => this.onSuccessItem(item, response, status, headers);
 
+  }
+
+  onSuccessItem(item: any, response: string, status: number, headers: any): any {
+    const data = JSON.parse(response); // success server response
+    this.progressBarService.addBackgroundImage({'planned': data.path });
+    console.log(data);
+}
+
+onErrorItem(item: any, response: string, status: number, headers: any): any {
+    const error = JSON.parse(response); // error server response
+    console.log(error);
+}
 }
