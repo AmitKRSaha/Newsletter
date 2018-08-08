e6ec63fb2df2f9727535396845a24856d7b99245
LL\amisaha <amisaha@publicisgroupe.net>
2018-08-08 14:47:32 +0530

    Added e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,42 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Newsletter!');
   });
+
+  it('should display business section message', () => {
+    page.navigateToBusiness();
+    expect(page.getBusinessParagraphText()).toEqual('Business Section');
+  });
+
+  it('should display planned section message', () => {
+    page.navigateToPlanned();
+    expect(page.getPlannedParagraphText()).toEqual('Planned Section');
+  });
+
+  it('should display technical section message', () => {
+    page.navigateToTech();
+    expect(page.getTechParagraphText()).toEqual('Technical Section');
+  });
+
+  it('should display brain teaser and healt tips section message', () => {
+    page.navigateToBrainteaserAndHealthtips();
+    expect(page.getBrainteaserAndHealthtipsParagraphText()).toEqual('Brain Teaser and Health Tips');
+  });
+
+  it('should display brain teaser answer section message', () => {
+    page.navigateToBrainteaserAnswer();
+    expect(page.getBrainteaserAnswerParagraphText()).toEqual('Brain Teaser Answer');
+  });
+
+  it('should display team image section message', () => {
+    page.navigateToTeamImage();
+    expect(page.getTeamImageParagraphText()).toEqual('Team Section');
+  });
+
+  it('should display final newsletter section message', () => {
+    page.navigateToFinalNewsletter();
+    expect(page.getFinalNewsletterParagraphText()).toEqual('Final News Letter');
+  });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,54 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  navigateToBusiness() {
+    return browser.get('/business');
+  }
+  getBusinessParagraphText() {
+    return element(by.css('app-businessupdate-preview h3')).getText();
+  }
+
+  navigateToPlanned() {
+    return browser.get('/planned');
+  }
+  getPlannedParagraphText() {
+    return element(by.css('app-project-preview h3')).getText();
+  }
+
+  navigateToTech() {
+    return browser.get('/technical');
+  }
+  getTechParagraphText() {
+    return element(by.css('app-technical-preview h3')).getText();
+  }
+
+  navigateToBrainteaserAndHealthtips() {
+    return browser.get('/brainteaserAndHealthtips');
+  }
+  getBrainteaserAndHealthtipsParagraphText() {
+    return element(by.css('app-barinteaser-healthtips h3')).getText();
+  }
+
+  navigateToBrainteaserAnswer() {
+    return browser.get('/brainteserans');
+  }
+  getBrainteaserAnswerParagraphText() {
+    return element(by.css('app-barinteaser-answer h3')).getText();
+  }
+
+  navigateToTeamImage() {
+    return browser.get('/teamimage');
+  }
+  getTeamImageParagraphText() {
+    return element(by.css('app-team-preview h3')).getText();
+  }
+
+  navigateToFinalNewsletter() {
+    return browser.get('/newsletter');
+  }
+  getFinalNewsletterParagraphText() {
+    return element(by.css('app-final-newsletter h1')).getText();
+  }
+
 }
