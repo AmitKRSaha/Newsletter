8a0ac8d71be700c812eb0d377c62ba6044357ed4
LL\amisaha <amisaha@publicisgroupe.net>
2018-08-09 17:12:25 +0530

    Updated with e2e Test for all the fields

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,110 @@
 import { AppPage } from './app.po';
 import { element, by, browser } from '../../node_modules/protractor';
 
+describe('workspace-project App should have different sections with all input properties', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+    page.navigateTo();
+  });
+
+  it('should have different sections', () => {
+    expect(element(by.css('.headerSection')).getText()).toEqual('Section Header Image');
+    expect(element(by.css('.businessUpdate')).getText()).toEqual('Bussiness Update Section');
+    expect(element(by.css('.opportunitySection')).getText()).toEqual('Opportunitis In PipeLine');
+    expect(element(by.css('.acheivementSection')).getText()).toEqual(' Acheivement '.trim());
+    expect(element(by.css('.plannedSection')).getText()).toEqual(' Planned '.trim());
+    expect(element(by.css('.projstatSection')).getText()).toEqual(' Project Statistic '.trim());
+    expect(element(by.css('.techSection')).getText()).toEqual(' Technical Arcitle '.trim());
+    expect(element(by.css('.brainSection')).getText()).toEqual(' Brain Teasers '.trim());
+    expect(element(by.css('.healthtipsSection')).getText()).toEqual(' Health Tips '.trim());
+    expect(element(by.css('.brainteaserSection')).getText()).toEqual(' Brain Teaser Answers '.trim());
+    expect(element(by.css('.teamSection')).getText()).toEqual(' Team Photo , Birthday and Anniversary '.trim());
+  });
+
+  it('should have header section with all properties', () => {
+    expect(element(by.css('app-section-head #schuploadButton'))).toBeTruthy();
+    expect(element(by.css('app-section-head #btnsectionHead'))).toBeTruthy();
+
+  });
+
+  it('should have business update section with all properties', () => {
+    expect(element(by.css('app-business-update input[type="file"]'))).toBeTruthy();
+    expect(element(by.css('app-business-update input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-business-update input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreBusinessUpdate'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreBusinessUpdate'))).toBeTruthy();
+  });
+
+  it('should have opportunity section with all properties', () => {
+    expect(element(by.css('app-opportunity-pipeline input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-opportunity-pipeline input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnOpportunity'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreOpportunity'))).toBeTruthy();
+  });
+
+  it('should have acheivement section with all properties', () => {
+    expect(element(by.css('app-acheivement input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-acheivement input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnAcheivement'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreAcheivement'))).toBeTruthy();
+  });
+
+  it('should have planned section with all properties', () => {
+    expect(element(by.css('app-planned input[type="file"]'))).toBeTruthy();
+    expect(element(by.css('app-planned input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-planned input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnPlanned'))).toBeTruthy();
+    expect(element(by.id('#btnAddMorePlannedUpdate'))).toBeTruthy();
+  });
+
+  it('should have project statistics section with all properties', () => {
+    expect(element(by.css('app-project-statistics input[type="text"]'))).toBeTruthy();
+    expect(element(by.css('app-project-statistics input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-project-statistics input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnProjectStat'))).toBeTruthy();
+  });
+
+  it('should have technical section with all properties', () => {
+    expect(element(by.css('app-technical-article input[type="file"]'))).toBeTruthy();
+    expect(element(by.css('app-technical-article input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-technical-article input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnTech'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreTechUpdate'))).toBeTruthy();
+  });
+
+  it('should have brain teaser section with all properties', () => {
+    expect(element(by.css('app-brain-teasers input[type="file"]'))).toBeTruthy();
+    expect(element(by.css('app-brain-teasers input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-brain-teasers input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnBrainTeaser'))).toBeTruthy();
+    expect(element(by.id('#btnAddMorebrainSection'))).toBeTruthy();
+  });
+
+  it('should have healthy tips section with all properties', () => {
+    expect(element(by.css('app-healthy-tips input[type="text"]'))).toBeTruthy();
+    expect(element(by.css('app-healthy-tips input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-healthy-tips input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnHealthTips'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreHealthTips'))).toBeTruthy();
+  });
+
+  it('should have brain teaser asnwer section with all properties', () => {
+    expect(element(by.css('app-brain-teaser-answer input[type="file"]'))).toBeTruthy();
+    expect(element(by.css('app-brain-teaser-answer input[type="button"]'))).toBeTruthy();
+    expect(element(by.css('app-brain-teaser-answer input[type="submit"]'))).toBeTruthy();
+    expect(element(by.id('#btnBrainTeaserAnswer'))).toBeTruthy();
+    expect(element(by.id('#btnAddMoreBrainTeaserAnswer'))).toBeTruthy();
+  });
+
+  it('should have teamimage section with all properties', () => {
+    expect(element(by.css('app-teamphoto-birthday input[type="file"]'))).toBeTruthy();
+    expect(element(by.id('#btnTeampic'))).toBeTruthy();
+  });
+
+});
+
 describe('workspace-project App with different navigation', () => {
   let page: AppPage;
 
@@ -62,13 +166,13 @@ describe('workspace-project App with image upload', () => {
     path = require('path');
   });
 
-  it('should upload section head image', function() {
+  it('should upload section head image', function () {
     page.navigateTo();
 
     expect(element(by.id('biz-text')).isPresent()).toBeFalsy();
 
     const fileToUpload =
-        '../../../form-app/src/assets/baseline-question_answer-24px.svg',
+      '../../../form-app/src/assets/baseline-question_answer-24px.svg',
       absolutePath = path.resolve(__dirname, fileToUpload);
     // browser.pause();
     element(by.css('app-section-head input')).sendKeys(absolutePath);
